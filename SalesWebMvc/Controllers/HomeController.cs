dbbce786d53d90861d755e5548b2ebf4bdf5f0dc
David Soares <dcunhasoares@gmail.com>
2022-01-09 11:24:40 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App from C# Course.";
+            ViewData["Author"] = "David da Cunha Soares";
 
             return View();
         }
