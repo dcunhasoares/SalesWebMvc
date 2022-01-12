8abcb7c82e7b0c08f0413a14926a9b79f6f187e3
David Soares <dcunhasoares@gmail.com>
2022-01-12 09:22:47 -0300

    Revert "First controller and Razor pages tests"
    This reverts commit dbbce786d53d90861d755e5548b2ebf4bdf5f0dc.

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -18,8 +18,7 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Sales Web MVC App from C# Course.";
-            ViewData["Author"] = "David da Cunha Soares";
+            ViewData["Message"] = "Your application description page.";
 
             return View();
         }
